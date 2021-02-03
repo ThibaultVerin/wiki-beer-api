afafc20a1058f96753858a220385b7ef33eba7d3
Thibault Verin <thibaultverin@hotmail.com>
2021-02-03 12:44:58 +0100

    Controllers done

---
+++ controllers/beer.js
@@ -0,0 +1,50 @@
+const {
+  getAllBeer,
+  findById,
+  postOneBeer,
+  putOneBeer,
+  deleteOneBeer,
+} = require('../models/beer');
+
+module.exports.handleBeerGetAll = async (req, res) => {
+  const rawData = await getAllBeer();
+  res.send(rawData);
+};
+
+module.exports.handleBeerGetOne = async (req, res) => {
+  res.send(await findById(req.params.id));
+};
+
+module.exports.handleBeerPost = async (req, res) => {
+  const { name, brand, country, price, type, description, advice } = req.body;
+  const data = await postOneBeer({
+    name,
+    brand,
+    country,
+    price,
+    type,
+    description,
+    advice,
+  });
+  return res.status(201).send(data);
+};
+
+module.exports.handleBeerPutOne = async (req, res) => {
+  const { name, brand, country, price, type, description, advice } = req.body;
+  const attribute = {
+    name,
+    brand,
+    country,
+    price,
+    type,
+    description,
+    advice,
+  };
+  const data = await putOneBeer(req.params.id, attribute);
+  res.send(data);
+};
+
+module.exports.handleBeerDeleteOne = async (req, res) => {
+  await deleteOneBeer(req.params.id);
+  res.sendStatus(204);
+};
